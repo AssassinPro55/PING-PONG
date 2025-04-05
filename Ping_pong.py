165bf9859575e9265d0f49c5e02d88410ae0cbc0
ASSASSIN <ruslanlatypov913@gmail.com>
2025-04-05 14:16:53 +0300

    Add files via upload

--- Ping_pong.py
+++ Ping_pong.py
@@ -26,7 +26,7 @@ class GameSprite(sprite.Sprite):
     def reset(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
 
-class Player(GameSprite):
+class Player1(GameSprite):
     def update(self):
         keys = key.get_pressed()
         if keys[K_w] and self.rect.y > 0:
@@ -34,23 +34,40 @@ class Player(GameSprite):
         if keys[K_s] and self.rect.y < H - 60:
             self.rect.y += self.speed
 
-class Enemy(GameSprite):
+class Player2(GameSprite):
     def update(self):
         keys = key.get_pressed()
-        if keys[K_o] and self.rect.y > 0:
+        if keys[K_UP] and self.rect.y > 0:
             self.rect.y -= self.speed
-        if keys[K_l] and self.rect.y < H - 60:
+        if keys[K_DOWN] and self.rect.y < H - 60:
             self.rect.y += self.speed
+
+class Ping_Ball(GameSprite):
+    def update(self):
+        pass
     
     def reset(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
 
 #Текст
 
-player = Player('Player.png', 0, 0, 85, 85, 8)
-monster = Enemy('Bot.png', 0, 700, 85, 85, 8)
+player1 = Player1('Player.png', 0, H//2, 85, 85, 8)
+player2 = Player2('Bot.png', 620, H//2, 85, 85, 8)
+ball = Ping_Ball('Ball.png', W//2, H//2, 60, 60, 0)
 
-####### 
+#Музыка
+
+mixer.init()
+mixer.music.load('blinding-lights.ogg')
+mixer.music.play()
+mixer.music.set_volume(0.5)
+
+kick = mixer.Sound('Ydar.ogg')
+
+#Игровой цикл
+
+speed_x = 5
+speed_y = 5
 
 game = True
 finish = False
@@ -59,13 +76,32 @@ while game:
         if e.type == QUIT:
             game = False
 
+    if (sprite.collide_rect(player1, ball)) or (sprite.collide_rect(player2, ball)):
+        mixer.music.load('Ydar.ogg')
+        mixer.music.play()
+
     if finish != True:
         window.fill(background)
-        player.update()
-        monster.update()
 
-        player.reset()
-        monster.reset()
+        ball.rect.y += speed_y
+        ball.rect.x += speed_x
+        
+        player1.update()
+        player2.update()
+        ball.update()
+
+        player1.reset()
+        player2.reset()
+        ball.reset()
+
+        #Отскок
+
+        if ball.rect.y > H-50 or ball.rect.y < 0:
+            speed_y += -1
+
+        if sprite.collide_rect(player1, ball) or sprite.collide_rect(player2, ball):
+            speed_x += -1
+            speed_y += 1
     
     display.update()
     clock.tick(FPS)
@@ -80,61 +116,20 @@ while game:
 
 
 
-
-
-
-
-
-    def draw_wall(self):
-        window.blit(self.image, (self.rect.x, self.rect.y))
-
-wall1 = Wall(131, 240, 6, 100, 20, 450, 10)
-wall2 = Wall(131, 240, 6, 100, 240, 450, 10)
-wall3 = Wall(131, 240, 6, 100, 240, 10, 450)
-
-
-
-mixer.init()
-mixer.music.load('jungles.ogg')
-mixer.music.play()
-
-kick = mixer.Sound('kick.ogg')
-money = mixer.Sound('money.ogg')
-
 # Текст
-font.init()
-font = font.Font(None, 70)
-win = font.render('YOU SIGMA!', True, (255, 215, 0))
-lose = font.render("YOU LOX!(((", True, (180, 0, 0))
+#font.init()
+#font = font.Font(None, 70)
+#win = font.render('YOU SIGMA!', True, (255, 215, 0))
+#lose = font.render("YOU LOX!(((", True, (180, 0, 0))
 
-player = Player('hero.png', 5, H - 80, 4)
-monster = Enemy('cyborg.png', W - 80, 280, 6)
-final = GameSprite('treasure.png', W - 120, H - 80, 0)
-
-
-game = True
-finish = False
-while game:
-    for e in event.get():
-        if e.type == QUIT:
-            game = False
+#player = Player('hero.png', 5, H - 80, 4)
+#monster = Ene#y('cyborg.png', W - 80, 280, 6)
+#final = GameSprite('treasure.png', W - 120, H - 80, 0)
 
 
-        wall1.draw_wall()
-        wall2.draw_wall()
-        wall3.draw_wall()
-
-        if sprite.collide_rect(player, final):
-            finish = True
-            window.blit(win, (200, 200))
-            money.play()
-
-        if (sprite.collide_rect(player, monster) or
-        sprite.collide_rect(player, wall1) or
-        sprite.collide_rect(player, wall2) or
-        sprite.collide_rect(player, wall3)):
-            window.blit(lose, (200, 200))
-            finish = True
-            kick.play()
+        #if sprite.collide_rect(player, final):
+            #finish = True
+            #window.blit(win, (200, 200))
+            #money.play()
             
 
